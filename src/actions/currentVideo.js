13c3bb3c35e4ed8237af9d47d6273f217efbc987
Chen Wilson <chenlwilson@gmail.com>
2018-11-17 11:59:28 -0800

    Complete defining action creators

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,35 @@
-var changeVideo = (video) => ({
+var changeVideo = (video) => {
   //TODO:  Return some action object to change the currently playing video.
-});
+  return {
+    type: 'CURRENT_VIDEO',
+    video
+  };
+
+};
 
 export default changeVideo;
+
+
+// const defaultState = {
+//   login: false
+// };
+
+// const reducer = (state = defaultState, action) => {
+//   // change code below this line
+//   if (action.type === 'LOGIN') {
+//     return  {
+//     login: true
+//     }
+//     } else {
+//       return defaultState
+//   };
+//   // change code above this line
+// };
+
+// const store = Redux.createStore(reducer);
+
+// const loginAction = () => {
+//   return {
+//     type: 'LOGIN'
+//   }
+// };
